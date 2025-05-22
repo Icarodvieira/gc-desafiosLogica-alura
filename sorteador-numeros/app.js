255e87b3a1861648f93da02184a368af75f961c8
icarodvieira <icarodvieira3@gmail.com>
2025-05-21 23:47:46 -0300

    Validação para quantidade de numeros vs tamanho do intervalo

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -4,10 +4,26 @@ function sortear(){
     let quantidadeNumeros = document.getElementById('quantidade').value;
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    console.log(de);
+    console.log(ate);
+    console.log(de >= ate);
+    
+    
+    if(de >= ate){
+        alert("O valor do primeiro numero do intervalo deve ser menor que o segundo.");
+        return;
+    }
+
+    if(quantidadeNumeros > (ate - de) + 1){
+        alert("A quantidade de números a serem sorteados deve ser menor ou igual o intervalo.");
+        return;
+    }
+
     if(quantidadeNumeros < 0 || quantidadeNumeros > 15){
         alert("A quantidade deve estar entre 1 e 15");
         return;
     }
+
     if(de < 1 || de > 100 || ate < 1 || ate > 100){
         alert("O intervalo máximo permitido é entre 1 e 100."); // mudar
         return;
