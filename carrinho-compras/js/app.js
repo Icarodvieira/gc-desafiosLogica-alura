a8c6fe510723db870ae06864ba8783cef51a0450
icarodvieira <icarodvieira3@gmail.com>
2025-06-02 19:58:54 -0300

    carrinho de compras

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,12 +1,25 @@
 
+let total;
+limpar();
 function adicionar(){
-    produto = document.getElementById('produto').value;
-    nomeProduto = (document.getElementById('produto').value).split('-')[0].trim();
-    valorUnitario = (document.getElementById('produto').value).split('R$')[1].trim();
-    qtd = parseInt(document.getElementById('quantidade').value);
+    let nomeProduto = (document.getElementById('produto').value).split('-')[0].trim();
+    let valorUnitario = parseInt((document.getElementById('produto').value).split('R$')[1].trim());
+    let qtd = parseInt(document.getElementById('quantidade').value);
     
     let preco = quantidade.value * valorUnitario;
-    alert(preco);
     
-    // document.querySelector('.carrinho__produtos__produto').innerHTML = `<section class="carrinho__produtos__produto"><span class="texto-azul">${qtd}x</span> ${produto} <span class="texto-azul">${valor}</span> </section>`
+    let carrinho = document.getElementById('lista-produtos');
+    
+        carrinho.innerHTML = carrinho.innerHTML + 
+    `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${qtd}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
+    </section>`;
+    total += preco;
+    document.getElementById('valor-total').textContent = `R$ ${total}`;
+}
+
+function limpar(){
+    total = 0;
+    document.querySelector('.carrinho__produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
